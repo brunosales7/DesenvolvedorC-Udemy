137aa523533a632d33adaa70aa0cb53f0d1f6bf9
brunosales7 <bruno_sales7@hotmail.com>
2022-07-28 00:31:45 -0300

    TiposEVariaveis

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Program
+    {
+
+        //Método Main:
+        //Ponto de entrada para a aplicação, ou seja é 
+        // o que define o início da aplicação
+
+        static void Main(string[] args)
+        {
+
+            //Imprimir na tela o famoso "Olá, Mundo!"
+            Console.WriteLine("Olá Mundo!");
+
+
+
+
+        }
+    }
+}
